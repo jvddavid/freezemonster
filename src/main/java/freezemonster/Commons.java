5494897fe357d249ba4590ac120f388645b248d3
Joao Victor Davi de Oliveira <j.victordavid2@gmail.com>
2025-05-07 08:12:53 -0300

    adiciona interface Commons com constantes do jogo

---
+++ src/main/java/freezemonster/Commons.java
@@ -0,0 +1,19 @@
+package freezemonster;
+
+public interface Commons extends spriteframework.Commons {
+
+    int TIRO_HEIGHT = 5;
+
+    int MONSTRO_HEIGHT = 30;
+    int MONSTRO_WIDTH = 30;
+    int MONSTRO_INIT_X = 150;
+    int MONSTRO_INIT_Y = 0;
+
+    int NUMBER_OF_MONSTERS_TO_DESTROY = 5;
+    int CHANCE = 5;
+    int PLAYER_WIDTH = 30;
+    int PLAYER_HEIGHT = 60;
+
+    // Adiciona uma constante para a velocidade base dos monstros
+    int MONSTER_BASE_SPEED = 2;
+}
